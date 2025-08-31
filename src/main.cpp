f5a3197e4f558f7d78f3cf8507a90572a71ce5ba
palaniraja <palaniraja@live.com>
2025-08-31 09:49:40 -0700

    simple pwm gen

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,34 @@
 #define GPIO_TX 21
 #define GPIO_LED 8
 
+#define GPIO_IN_TEST GPIO_LED
+
+#define DUTY_CYCLE 255
+
 void setup() {
     Serial.begin(115200);
     Serial.println("Setup done");
+
+    pinMode(GPIO_IN_TEST, OUTPUT);
+
 }
 
 
 void loop() {
     Serial.println("loop");
+    // increase the LED brightness
+    for (int dutyCycle = 0; dutyCycle <= DUTY_CYCLE; dutyCycle++)
+    {
+        // changing the LED brightness with PWM
+        analogWrite(GPIO_IN_TEST, dutyCycle);
+        delay(15);
+    }
+
+    // decrease the LED brightness
+    for (int dutyCycle = DUTY_CYCLE; dutyCycle >= 0; dutyCycle--)
+    {
+        // changing the LED brightness with PWM
+        analogWrite(GPIO_IN_TEST, dutyCycle);
+        delay(15);
+    }
 }
