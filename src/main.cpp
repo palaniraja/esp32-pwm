01e51f635cc4de9cef94386066c27627da7b7266
palaniraja <palaniraja@live.com>
2025-09-07 19:23:12 -0700

    sanity check to see if i blew up gpio pin

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,8 @@
 #define GPIO_TX 21
 #define GPIO_LED 8
 
+#define GPIO_IN_TEST_MIRROR 3
+
 #define GPIO_IN_TEST GPIO_LED
 
 #define DUTY_CYCLE 255
@@ -12,7 +14,7 @@ void setup() {
     Serial.println("Setup done");
 
     pinMode(GPIO_IN_TEST, OUTPUT);
-
+    pinMode(GPIO_IN_TEST_MIRROR, OUTPUT);
 }
 
 
@@ -23,6 +25,7 @@ void loop() {
     {
         // changing the LED brightness with PWM
         analogWrite(GPIO_IN_TEST, dutyCycle);
+        analogWrite(GPIO_IN_TEST_MIRROR, dutyCycle);
         delay(15);
     }
 
@@ -31,6 +34,7 @@ void loop() {
     {
         // changing the LED brightness with PWM
         analogWrite(GPIO_IN_TEST, dutyCycle);
+        analogWrite(GPIO_IN_TEST_MIRROR, dutyCycle);
         delay(15);
     }
 }
